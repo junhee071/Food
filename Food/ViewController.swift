d8b93363b010a86160f02b00dce4d33e28acfc9e
Jun Hee Han <junhee071@gmail.com>
2018-07-23 11:45:25 -0700

    Initial Commit

---
+++ Food/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food
+//
+//  Created by Jun Hee Han on 7/23/18.
+//  Copyright © 2018 Jun Hee Han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
